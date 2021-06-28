85e3b57da5f1b58932f24a007b1bec1c8c66eae2
CrimzPhoenix <86602110+CrimzPhoenix@users.noreply.github.com>
2021-06-28 19:15:22 +0800

    Update builtInSets.js

--- src/builtInSets.js
+++ src/builtInSets.js
@@ -1,6 +1,6 @@
 var builtInSets = [
-    {
-        name: "Duskit",
+    duskit: {
+    name: "Duskit",
         setName: "Peace of Mind",
         evs: {
             hp: 100,
@@ -31,38 +31,5 @@ var builtInSets = [
         ability: "None",
         item: "Power Cuffs",
         level: 50
-    }
-    {
-        name: "Duskit",
-        setName: "Peace of Mind",
-        evs: {
-            hp: 100,
-            energy: 0,
-            attack: 0,
-            defense: 0,
-            attackR: 200,
-            defenseR: 0,
-            speed: 200
-        },
-        ivs: {
-            hp: 40,
-            energy: 40,
-            attack: 40,
-            defense: 40,
-            attackR: 40,
-            defenseR: 40,
-            speed: 40
-        },
-        moves: {
-            move1: "Spectral Burst",
-            move2: "Brainwash",
-            move3: "Peace Of Mind",
-            move4: "Final Ruse"
-        },
-        posNature: "nimble",
-        negNature: "none",
-        ability: "None",
-        item: "Power Cuffs",
-        level: 50
-    }
+    },
 ];
